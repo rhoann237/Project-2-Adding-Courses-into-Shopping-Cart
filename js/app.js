c04fc4106847f534adced5626f8582e68bd7dc1d
Rhoann Mae Galicio <rhoannmaegalicio@gmail.com>
2021-04-29 22:42:50 +0800

    Adding Courses into the Shopping Cart Part 2

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // Variables
-const courses = document.querySelector('#courses-list');
+const courses = document.querySelector('#courses-list'),
+        shoppingCartContent = document.querySelector('#cart-content tbody');
 
 
 
@@ -48,7 +49,19 @@ function addIntoCart(course) {
 
     // Build the template
     row.innerHTML = `
-    
-    
+        <tr>
+            <td>
+                <img src="${course.image}" width=100>
+            </td>
+            <td>${course.title}</td>
+            <td>${course.price}</td>
+            <td>
+                <a href="#" class="remove" data-id="${course.id}">X</a>
+            </td>
+
+
+        </tr>
     `;
+    // add into the shopping cart
+    shoppingCartContent.appendChild(row);
 }
